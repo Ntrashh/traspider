e0c08e86bb26bd2dcba0f3fc2216d77242d55550
DESKTOP-JLAI026\50247 <502472403@qq.com>
2022-12-15 15:06:04 +0800

    no message

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 import os
+import re
 
 from setuptools import find_packages, setup
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+
+def read_version():
+    regexp = re.compile(r'^__version__\W*=\W*"([\d.abrc]+)"')
+    init_py = os.path.join(os.path.dirname(__file__), "traspider", "__init__.py")
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+
 def read(file_name):
     with open(
         os.path.join(os.path.dirname(__file__), file_name), mode="r", encoding="utf-8"
@@ -33,7 +46,7 @@ requires = [
 
 setup(
     name="traspider",
-    version='0.0.1',
+    version=read_version(),
     author="NTrash",
     author_email='yinghui0214@163.com',
     description="An out-of-the-box lightweight asynchronous crawler framework",
