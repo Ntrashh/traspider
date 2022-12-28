f1684cd448584ea025d8f765950cccb80f75d07f
notrash <502472403@qq.com>
2022-12-28 18:44:57 +0800

    修改md5为staticmethod方法

--- traspider/util/encrypt.py
+++ traspider/util/encrypt.py
@@ -3,8 +3,8 @@ import hashlib
 
 class Encrypt:
 
-
-	def md5(self,str):
+	@staticmethod
+	def md5(str):
 		hashlib_md5 = hashlib.md5()
 		hashlib_md5.update(str.encode(encoding='utf-8'))
 		return hashlib_md5.hexdigest()
